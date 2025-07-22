c991b6ccd59f6b20f7c36dfa3dec05301c6192e1
amorris <amorris@amorris.cc>
2025-07-22 14:49:47 -0400

    moving circle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
+use bevy::prelude::*;
+use bevy::color::palettes::basic;
 fn main() {
-    println!("Hello, world! This is a simple Rust program.")
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins);
+
+    app.add_systems(Startup, setup);
+    app.add_systems(Update, move_circle);
+
+    app.run();
 }
 
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>
+    ) 
+{
+    commands.spawn(Camera2d);
+    let circle = Mesh2d(meshes.add(Circle::new(100.)));
+    let cir_mat = MeshMaterial2d(materials.add(Color::from(basic::PURPLE)));
+    let transform = Transform::from_xyz(0., 0., 0.);
+
+    commands.spawn((circle, cir_mat, transform));
+}
+
+fn move_circle(
+    mut circle: Query<(&Mesh2d, &mut Transform)>,
+)
+{
+    for (_, mut transform) in &mut circle {
+        transform.translation.x += 10.;
+    }
+}
