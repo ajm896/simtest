275ecf99411e5a24bbc0d2d1dcce1ec2a8acc15a
amorris <amorris@amorris.cc>
2025-07-28 19:30:13 -0400

    moving ball and velocity marker

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use bevy::prelude::*;
 use bevy::color::palettes::basic;
+use bevy::prelude::*;
+
+#[derive(Component, Deref, DerefMut)]
+struct Position(Vec3);
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec3);
+#[derive(Component)]
+struct Ball;
+#[derive(Component)]
+struct Paddle;
+#[derive(Component)]
+struct Player;
+
+const WINDOW_WIDTH: f32 = 800.;
+const WINDOW_HEIGHT: f32 = 600.;
+
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins(DefaultPlugins);
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+            title: "PONG".into(),
+            ..default()
+        }),
+        ..default()
+    }));
 
     app.add_systems(Startup, setup);
-    app.add_systems(FixedUpdate, move_circle);
+    app.add_systems(Update, move_ball);
 
     app.run();
 }
@@ -14,22 +36,28 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>
-    ) 
-{
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn(Camera2d);
-    let circle = Mesh2d(meshes.add(Circle::new(10.)));
-    let cir_mat = MeshMaterial2d(materials.add(Color::from(basic::PURPLE)));
+    let ball = Mesh2d(meshes.add(Rectangle::new(10., 10.)));
+    let ball_mat = MeshMaterial2d(materials.add(Color::from(basic::WHITE)));
     let transform = Transform::from_xyz(0., 0., 0.);
+    let init_vel = Velocity(vec3(0.5, 1., 0.));
 
-    commands.spawn((circle, cir_mat, transform));
+    commands.spawn((ball, ball_mat, transform, init_vel, Ball));
 }
 
-fn move_circle(
-    mut circle: Query<(&Mesh2d, &mut Transform)>,
-)
-{
-    for (_, mut transform) in &mut circle {
-        transform.translation.x += 10.;
+fn move_ball(mut gizmos: Gizmos, mut ball: Query<(&mut Velocity, &mut Transform), With<Ball>>) {
+    for (mut velocity, mut transform) in &mut ball {
+        transform.translation += velocity.0;
+        gizmos.ray(transform.translation, velocity.0 * 10., basic::LIME);
+        if transform.translation.x >= WINDOW_WIDTH / 2. - 5. {
+            velocity.0 = Vec3::ZERO;
+            transform.translation.x = WINDOW_WIDTH / 2. - 5.; // Clamp at boundary
+        }
+        else if transform.translation.y >= WINDOW_HEIGHT / 2. - 5. {
+            velocity.0 = Vec3::ZERO;
+            transform.translation.y = WINDOW_HEIGHT / 2. - 5.; // Clamp at boundary
+        }
     }
 }
