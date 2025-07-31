15f4f21645e6c6ee35915382a81a95f2e83871f1
amorris <amorris@amorris.cc>
2025-07-31 07:32:41 -0400

    Paddle collision

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,16 @@ struct Ball;
 struct Paddle;
 #[derive(Component)]
 struct Player;
+#[derive(Component)]
+struct Collider;
 
 const WINDOW_WIDTH: f32 = 800.;
 const WINDOW_HEIGHT: f32 = 600.;
 const BALL_SIZE: f32 = 10.;
 const BALL_SPEED: f32 = 5.;
+
+const PADDLE_WIDTH: f32 = BALL_SIZE;
+const PADDLE_HEIGHT: f32 = BALL_SIZE * 10.;
 
 fn main() {
     let mut app = App::new();
@@ -39,22 +44,49 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
 
-fn move_ball(mut gizmos: Gizmos, mut ball: Query<(&mut Velocity, &mut Transform), With<Ball>>) {
+fn move_ball(mut gizmos: Gizmos, 
+    mut ball: Query<(&mut Velocity, &mut Transform), With<Ball>>,
+    paddles : Query<(&Collider, &Transform), Without<Ball>>) {
     for (mut velocity, mut transform) in &mut ball {
+        // Move ball
         transform.translation += velocity.0.extend(0.);
+
+        // helper
         gizmos.ray(
             transform.translation,
             velocity.0.extend(0.) * 10.,
             basic::LIME,
         );
+
+        // Wall Collison
         if transform.translation.x >= WINDOW_WIDTH / 2. - 5. {
-            velocity.0 = velocity.0.reflect(Vec2 { x: -1., y: 0. });
+            velocity.0 = velocity.0.reflect(Vec2::NEG_X);
         } else if transform.translation.y >= WINDOW_HEIGHT / 2. - 5. {
-            velocity.0 = velocity.0.reflect(Vec2 { y: -1., x: 0. });
+            velocity.0 = velocity.0.reflect(Vec2::NEG_Y);
         } else if transform.translation.x <= -WINDOW_WIDTH / 2. + 5. {
-            velocity.0 = velocity.0.reflect(Vec2 { x: -1., y: 0. });
+            velocity.0 = velocity.0.reflect(Vec2::NEG_X);
         } else if transform.translation.y <= -WINDOW_HEIGHT / 2. + 5. {
-            velocity.0 = velocity.0.reflect(Vec2 { y: -1., x: 0. });
+            velocity.0 = velocity.0.reflect(Vec2::NEG_Y);
+        }
+        // Paddle Collison
+        for (_paddle_coll, paddle_loc) in paddles {
+            let ball_x = transform.translation.x;
+            let ball_y = transform.translation.y;
+            let paddle_x = paddle_loc.translation.x;
+            let paddle_y = paddle_loc.translation.y;
+
+            let paddle_top = paddle_y + PADDLE_HEIGHT/2.;
+            let paddle_bottom = paddle_y - PADDLE_HEIGHT/2.;
+
+            let ball_top = ball_y + BALL_SIZE/2.;
+            let ball_bottom = ball_y - BALL_SIZE/2.;
+
+            let collision_x = ball_x == paddle_x; 
+            let collision_y = ball_top < paddle_top && ball_bottom > paddle_bottom;
+
+            if collision_x && collision_y {
+               velocity.0 = -velocity.0 
+            }
         }
     }
 }
@@ -64,10 +96,11 @@ fn make_ball(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let ball = Mesh2d(meshes.add(Rectangle::new(BALL_SIZE, BALL_SIZE)));
+    let shape = Rectangle::new(BALL_SIZE, BALL_SIZE);
+    let ball = Mesh2d(meshes.add(shape));
     let ball_mat = MeshMaterial2d(materials.add(Color::from(basic::WHITE)));
     let transform = Transform::from_xyz(0., 0., 0.);
-    let init_vel = Velocity(vec2(0.5, 1.).normalize() * BALL_SPEED);
+    let init_vel = Velocity(vec2(1., 1.) * BALL_SPEED);
 
     commands.spawn((ball, ball_mat, transform, init_vel, Ball));
 }
@@ -77,12 +110,13 @@ fn make_paddle(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let ball = Mesh2d(meshes.add(Rectangle::new(BALL_SIZE, BALL_SIZE * 10.)));
-    let ball_mat = MeshMaterial2d(materials.add(Color::from(basic::WHITE)));
+    let shape = Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT);
+    let paddle = Mesh2d(meshes.add(shape));
+    let paddle_mat = MeshMaterial2d(materials.add(Color::from(basic::WHITE)));
     let transform = Transform::from_xyz(350., 0., 0.);
     let init_vel = Velocity(vec2(0., 0.));
 
-    commands.spawn((ball, ball_mat, transform, init_vel, Ball));
+    commands.spawn((paddle, paddle_mat, transform, init_vel, Paddle, Collider));
 }
 
 fn make_cpu_paddle(
@@ -90,10 +124,11 @@ fn make_cpu_paddle(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let ball = Mesh2d(meshes.add(Rectangle::new(BALL_SIZE, BALL_SIZE * 10.)));
-    let ball_mat = MeshMaterial2d(materials.add(Color::from(basic::WHITE)));
+    let shape = Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT);
+    let paddle = Mesh2d(meshes.add(shape));
+    let paddle_mat = MeshMaterial2d(materials.add(Color::from(basic::WHITE)));
     let transform = Transform::from_xyz(-350., 0., 0.);
     let init_vel = Velocity(vec2(0., 0.));
 
-    commands.spawn((ball, ball_mat, transform, init_vel, Ball));
+    commands.spawn((paddle, paddle_mat, transform, init_vel, Paddle, Collider));
 }
